070221d4654e840e42bea0ea731255c2ea3fa48b
Daniela Fernandez <daniela.fernandez7@gmail.com>
2019-04-17 19:29:25 -0400

    desafios resutlos completo

--- filto_procesos.rb
+++ filto_procesos.rb
@@ -3,6 +3,7 @@ output_data = File.open 'procesos_filtrados.data', 'w'
 filter = ARGV[0].to_i
 input_data.readlines.each do |line|
   output_data.print line
-  if line.to_i > filter
+    if line.to_i > filter
+    end
   end
   output_data.close
